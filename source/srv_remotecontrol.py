fa2864eb02f605b61b926dcf34025aafeda5f6c5
Odroid-X2 <cameron.rodda@synchrotron.org.au>
2014-03-23 15:50:52 +1100

    fixed motor controller directions

--- source/srv_remotecontrol.py
+++ source/srv_remotecontrol.py
@@ -28,8 +28,8 @@ class RemoteControl(object):
 		accel = 0
 		
 		print(kws)
-		if "mode" in kws:
-			mode = str(kws['mode'])
+		if "Mode" in kws:
+			mode = str(kws['Mode'])
 		if "Lx" in kws:
 			lx = int(kws['Lx'])
 		if "Ly" in kws:
@@ -55,53 +55,38 @@ class RemoteControl(object):
 		#convert to a number between 0 and 100 or 0 and -100 for reverse.
 
 		#conversion from the arduino/ps2 controller
-		if mode == 'arduino':
-
+		#print("Checking for Arduino control...")
+		if mode == "Arduino":
+			print("Arduino in control")
 			quad = self.which_quadrant(lx,ly)
 
 			if quad == 1:
-				leftV = self.v_max
-				lfx = lx - self.neutral
-				lfy = self.neutral - ly
-				tmpDeg = math.degrees(math.atan2(lfy,lfx))
-				rightV = (tmpDeg / 90) * self.v_max
-
-			elif quad == 2:
 				rightV = self.v_max
-				lfx = self.neutral - lx
+				lfx = lx - self.neutral
 				lfy = self.neutral - ly
 				tmpDeg = math.degrees(math.atan2(lfy,lfx))
 				leftV = (tmpDeg / 90) * self.v_max
 
-			elif quad == 3:
-				rightV = self.v_min
+			elif quad == 2:
+				leftV = self.v_max
 				lfx = self.neutral - lx
-				lfy = ly - self.neutral
+				lfy = self.neutral - ly
 				tmpDeg = math.degrees(math.atan2(lfy,lfx))
-				leftV = (tmpDeg / 90) * self.v_min
+				rightV = (tmpDeg / 90) * self.v_max
 
-			elif quad == 4:
+			elif quad == 3:
 				leftV = self.v_min
-				lfx = lx - self.neutral
+				lfx = self.neutral - lx
 				lfy = ly - self.neutral
 				tmpDeg = math.degrees(math.atan2(lfy,lfx))
 				rightV = (tmpDeg / 90) * self.v_min
 
-			elif quad == 5:
-				leftV = self.v_max
-				rightV = self.v_max
-
-			elif quad == 6:
-                                leftV = self.v_min
-                                rightV = self.v_min
-
-			elif quad == 7:
-                                leftV = self.v_min
-                                rightV = self.v_max
-
-			elif quad == 8:
-                                leftV = self.v_max
-                                rightV = self.v_min
+			elif quad == 4:
+				rightV = self.v_min
+				lfx = lx - self.neutral
+				lfy = ly - self.neutral
+				tmpDeg = math.degrees(math.atan2(lfy,lfx))
+				leftV = (tmpDeg / 90) * self.v_min
 
 			else:  #neutral
 				leftV = 0
@@ -112,30 +97,28 @@ class RemoteControl(object):
 		msg = {'leftA': leftA, 'rightA': rightA, 'leftV': leftV, 'rightV': rightV}
 		#Send the zmq message tothe motor server
 		motors_socket.send_json(msg)
+		print("Message sent... leftMotor:%s rightMotor:%s" % (leftV, rightV))
 
 		return "Received... leftH:%s leftV:%s rightH:%s rightV:%s" % (lx, ly, rx, ry)
 
 	def which_quadrant(self, horiz, vert):
-		#quad = 0
-		
-		if ((vert > self.neutral_max) and (horiz > self.neutral_min) and (horiz < neutral_max)):
-			quad = 5	# Full steam ahead
-		if ((vert < self.neutral_min) and (horiz > self.neutral_min) and (horiz < neutral_max)):
-                        quad = 6        # Full steam behind
-		if ((horiz < self.neutral_min) and (vert > self.neutral_min) and (vert < neutral_max)):
-                        quad = 7        # Full spin left
-		if ((horiz > self.neutral_max) and (vert > self.neutral_min) and (vert < neutral_max)):
-                        quad = 8        # Full spin right
-		elif ((horiz > self.neutral_max) and (vert < self.neutral_min)):
-			quad = 1	# behind right
-		elif ((horiz < self.neutral_min) and (vert < self.neutral_min)):
-			quad = 2	# behind left
-		elif ((horiz < self.neutral_min) and (vert > self.neutral_max)):
-			quad = 3	# ahead left
-		elif ((horiz > self.neutral_max) and (vert > self.neutral_max)):
-			quad = 4	# ahead right
-		else:
-			quad = 0	# neutral, no move
+		quad = 0
+		print("vert:%s  horiz:%s" % (horiz, vert))
+		if ((horiz > self.neutral) and (vert < self.neutral)):
+			quad = 1	# ahead right
+			print("Quad 1  vert:%s  horiz:%s" % (horiz, vert))
+		if ((horiz < self.neutral) and (vert < self.neutral)):
+			quad = 2	# ahead left
+			print("Quad 2  vert:%s  horiz:%s" % (horiz, vert))
+		if ((horiz < self.neutral) and (vert > self.neutral)):
+			quad = 3	# behind left
+			print("Quad 3  vert:%s  horiz:%s" % (horiz, vert))
+		if ((horiz > self.neutral) and (vert > self.neutral)):
+			quad = 4	# behind right
+			print("Quad 4  vert:%s  horiz:%s" % (horiz, vert))
+		if ((horiz > self.neutral_min) and (horiz < self.neutral_max) and (vert > self.neutral_min) and (vert < self.neutral_max)):
+			quad = 0	# overwrite the neutral square
+			print("Quad 0  vert:%s  horiz:%s" % (horiz, vert))
 
 		return quad
 
