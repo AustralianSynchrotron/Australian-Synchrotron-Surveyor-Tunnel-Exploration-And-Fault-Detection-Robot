0ed9db0513f19b16fe7a401493d932962b8254de
Odroid-X2 <cameron.rodda@synchrotron.org.au>
2015-02-21 03:47:18 +1100

    fixed problems with recv_json hanging/ waiting for a message

--- source/test_remotecontrol_flask.py
+++ source/test_remotecontrol_flask.py
@@ -4,50 +4,65 @@ import time
 from threading import Thread
 from flask import Flask, render_template, session, request
 from flask.ext.socketio import SocketIO, emit, join_room, leave_room
+import logging
 
 import zmq
+
+logging.basicConfig(filename='/tmp/remote.log', level=logging.DEBUG)
 
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+
 thread = None
+
 def background_thread():
     context = zmq.Context()
     battery_socket = context.socket(zmq.SUB)
     battery_socket.connect("ipc:///tmp/battery.ipc")
     battery_socket.setsockopt(zmq.SUBSCRIBE, '') #subscribe to all messages
+    logging.debug("battery socket connected")
 
     bumper_socket = context.socket(zmq.SUB)
     bumper_socket.connect("ipc:///tmp/bumper.ipc")
     bumper_socket.setsockopt(zmq.SUBSCRIBE, '') #subscribe to all messages
+    bumper_socket.setsockopt(zmq.RCVTIMEO, 1000) #set time out in ms
+    logging.debug("bumper socket connected")
 
     """Example of how to send server generated events to clients."""
     count = 0
     while True:
-        time.sleep(10)
+        time.sleep(2)
         count += 1
         socketio.emit('my response',
             {'data': 'Server generated event', 'count': count},
             namespace='')
 
         msg = battery_socket.recv_json()
+        logging.debug("Message Recieved: %s" % msg)        
         print('Message Recieved: %s' % msg )
         socketio.emit('battery response',
             {'data': msg},
             namespace='')
 
-        bmp = bumper_socket.recv_json()
-        print('Message Recieved: %s' % bmp )
-        socketio.emit('bumper response',
-            {'data': bmp},
-            namespace='')
+        try:
+            # recv_json will block if no message available
+            # this message is interrupt triggered so not always a message available
+            bmp = bumper_socket.recv_json(zmq.NOBLOCK)
+            logging.debug("Message Recieved: %s" % bmp)
+            print('Message Recieved: %s' % bmp )
+            socketio.emit('bumper response',
+                {'data': bmp},
+                namespace='')
+        except:
+            continue
 
 @app.route('/')
 def index():
     global thread
-    #if thread is None:
-    if not thread.isAlive():
+    if thread is None:
+    #if not thread.isAlive():
         thread = Thread(target=background_thread)
         thread.start()
     return render_template('index.html')
