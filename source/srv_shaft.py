b58dcd2f81825ac5209897df6e16e809119e61d9
Odroid-X2 <cameron.rodda@synchrotron.org.au>
2015-04-20 05:38:50 +1000

    updated shaft code to match test suite

--- source/srv_shaft.py
+++ source/srv_shaft.py
@@ -7,7 +7,7 @@ http://creativecommons.org/licenses/by/2.5/ca/
 """
 
 
-import time
+#import time
 import sys
 import zmq
 
@@ -17,6 +17,7 @@ from time import sleep
 #import sys
 import random
 #Phidget specific imports
+from Phidgets.Phidget import PhidgetID
 from Phidgets.PhidgetException import PhidgetErrorCodes, PhidgetException
 from Phidgets.Events.Events import AttachEventArgs, DetachEventArgs, ErrorEventArgs
 from Phidgets.Devices.InterfaceKit import InterfaceKit
@@ -65,44 +66,13 @@ def interfaceKitError(e):
         print("Phidget Exception %i: %s" % (e.code, e.details))
 
 
-def interfaceKitLCDInputChanged(e):
-    source = e.device
-    print("InterfaceKitLCD %i: Input %i: %s" % (source.getSerialNum(), e.index, e.state))
-
-def interfaceKitSensorChanged(e):
-    source = e.device
-#    print e.rawValue
-    try:
-        if e.index >= 6:
-            #Sonar sensor range 0cm - 645cm
-            #Plugged into analog input 6 & 7 for N (forward) and S (reverse)
-            distance_mm = e.value * 12.96
-            if distance_mm > 6450.0:
-                distance_mm = "NaN"
-
-        else:
-            #IR range 20cm - 150cm
-            distance_mm = (9462/(e.value - 16.92)) * 10
-            #Not a reliable mesaurement below 200mm (20cm) or above 1500 (150cm)
-            if (distance_mm < 200.0) or (distance_mm > 1500.0):
-                distance_mm = "NaN"
-
-        print( "Distance - Device %i: %smm" % ( e.index, distance_mm ) )
-    except PhidgetException as e:
-        print("Phidget Exception %i: %s" % (e.code, e.details))
-
-#    print("InterfaceKit %i: Sensor %i: %i" % (source.getSerialNum(), e.index, e.value))
-
 def interfaceKitOutputChanged(e):
     source = e.device
     print("InterfaceKit %i: Output %i: %s" % (source.getSerialNum(), e.index, e.state))
 
 #Main Program Code
 try:
-
     interfaceKitLCD.setOnErrorhandler(interfaceKitError)
-    interfaceKitLCD.setOnInputChangeHandler(interfaceKitLCDInputChanged)
-
 except PhidgetException as e:
     print("Phidget Exception %i: %s" % (e.code, e.details))
     print("Exiting....")
@@ -113,24 +83,24 @@ print("Opening phidget object....")
 try:
 # Open interfaceKit by serial number to avoid conflicts with future interface kits...
 # As displayed by displayDeviceInfo()
-    interfaceKitLCD.openPhidget(serial=120517)
-
-    
+    interfaceKitLCD.openRemote('odroid',serial=120517)
 except PhidgetException as e:
     print("Phidget Exception %i: %s" % (e.code, e.details))
     print("Exiting....")
     exit(1)
 
-print("Waiting for attach....")
+if not interfaceKitLCD.isAttachedToServer():
+    sleep(2)
 
+print('interfaceKitLCD attached to server: %s' % interfaceKitLCD.isAttachedToServer())
+
+#wait for the device to attach
 try:
     interfaceKitLCD.waitForAttach(10000)
-
 except PhidgetException as e:
     print("Phidget Exception %i: %s" % (e.code, e.details))
     try:
         interfaceKitLCD.closePhidget()
-        
     except PhidgetException as e:
         print("Phidget Exception %i: %s" % (e.code, e.details))
         print("Exiting....")
@@ -140,6 +110,7 @@ except PhidgetException as e:
 else:
     displayDeviceInfo()
 
+print('lcd setup complete, ready for use')
 #Register interupt callback
 signal.signal(signal.SIGINT, signal_handler)
 
@@ -147,11 +118,10 @@ signal.signal(signal.SIGINT, signal_handler)
 context = zmq.Context()
 shaft_socket = context.socket(zmq.SUB)
 shaft_socket.bind("ipc:///tmp/shaft.ipc")
-
+shaft_socket.setsockopt(zmq.SUBSCRIBE, "") #subscribe to all messages
+print("SUB socket complete on ipc://tmp/shaft.ipc")
 
 while True:
-    message = []
-
     if interupt:
         print('Interupt: Shutting down script...')
         #try and shut down the interface kits
@@ -167,16 +137,36 @@ while True:
         exit(2)
         break
 
+    try:
+        relayed = shaft_socket.recv_json()
+        print(relayed)
 
-    #Export distance array via json to ipc://distance.ipc
-    msg = dict(zip(array,message))
-    print("Sending Message: %s" % msg )
-    print("==============================")
-    distance_socket.send_json(msg)
+        if 'down' in relayed:
+            interfaceKitLCD.setOutputState(0,True)
+            interfaceKitLCD.setOutputState(1,False)
+            print("going Down")
+            sleep(int(relayed['down']))
 
-#print "Sensor ValueLCD",interfaceKitLCD.getSensorValue(0)
+            interfaceKitLCD.setOutputState(0,False)
+            interfaceKitLCD.setOutputState(1,False)
 
-#print interfaceKitHUB.getRatiometric()
+        elif 'up' in relayed:
+            interfaceKitLCD.setOutputState(0,False)
+            interfaceKitLCD.setOutputState(1,True)
+            print("going Up")
+            sleep(int(relayed['up']))
+
+            interfaceKitLCD.setOutputState(0,False)
+            interfaceKitLCD.setOutputState(1,False)
+
+        else:
+            #default to stop all motion to avoid collisions
+            interfaceKitLCD.setOutputState(0,False)
+            interfaceKitLCD.setOutputState(1,False)
+
+    except:
+        print("nothing received... waiting")
+        pass
 
 print("Press Enter to quit....")
 
